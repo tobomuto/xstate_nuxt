4ba8b270984294181d7c128d3a96c3316018dee8
Thomas Bouvier <thomas@ylly.fr>
2025-07-18 10:36:38 +0200

    :wrench: Auto-import machines directory

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  imports: {
+    dirs: [
+      'machines', // Directory where your XState machines are located
+    ],
+  }
 })
